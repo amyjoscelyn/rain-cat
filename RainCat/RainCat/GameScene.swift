1fbe838f08c1c5a40a1b55c26012ccd4c8f8fc04
amyjoscelyn <amy.joscelyn@flatironschool.com>
2016-10-03 17:32:40 -0400

    unnecessary files and boilerplate code removed

---
+++ RainCat/RainCat/GameScene.swift
@@ -0,0 +1,45 @@
+//
+//  GameScene.swift
+//  RainCat
+//
+//  Created by Amy Joscelyn on 10/3/16.
+//  Copyright © 2016 Amy Joscelyn. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene
+{
+    private var lastUpdateTime : TimeInterval = 0
+    
+    override func sceneDidLoad()
+    {
+        self.lastUpdateTime = 0
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        
+    }
+    
+    override func update(_ currentTime: TimeInterval)
+    {
+        // Called before each frame is rendered
+        
+        // Initialize _lastUpdateTime if it has not already been
+        if (self.lastUpdateTime == 0)
+        {
+            self.lastUpdateTime = currentTime
+        }
+        
+        // Calculate time since last update
+        let dt = currentTime - self.lastUpdateTime        
+        self.lastUpdateTime = currentTime
+    }
+}
